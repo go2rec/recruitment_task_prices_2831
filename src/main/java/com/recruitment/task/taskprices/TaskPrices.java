dcfbac25410d305c9496779c2c9e8c308573dcad
go2rec <101624501+go2rec@users.noreply.github.com>
2023-01-09 10:14:18 +0100

    Update TaskPrices.java

--- src/main/java/com/recruitment/task/taskprices/TaskPrices.java
+++ src/main/java/com/recruitment/task/taskprices/TaskPrices.java
@@ -1,3 +1,12 @@
+//Task prices
+//Given an array of numbers representing stock prices of company in chronological order write a function 
+// that calculate maximum profit you could make from buying and selling the stock one time. 
+// You have to buy a stock before you can sell it.
+
+//Example:
+//X=[8,11,5,7,8,10] -> you should return the buy price 5 and sell price 10, which gives maximum profit of 5
+//X=[9,10,1] -> you should return the buy price 9 and sell price 10, which gives maximum profit of 1
+
 package com.recruitment.task.taskprices;
 
 public class TaskPrices {
